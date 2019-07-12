dc2c4c060c9b05c1c1c5b255a1e8ff85af272e51
jtnjnty <ojhon@live.com>
2019-07-11 22:17:02 -0300

    Running examples with mvn clean install command

--- src/main/java/com/joinesty/Main.java
+++ src/main/java/com/joinesty/Main.java
@@ -5,7 +5,7 @@ import com.joinesty.examples.Examples;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    String apiKey = System.getenv("API_KEY");
+    String apiKey = System.getenv("NULLAFI_API_KEY");
     if (apiKey == null) throw new RuntimeException("Environment API_KEY could not be null.");
 
     Examples examples = new Examples(apiKey);
